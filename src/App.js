210587fd729478e7925f311bf8f6f33a728ed933
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-09-08 14:29:57 +0200

    testing

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import FirstName from "./components/FirstName";
+import LastName from "./components/LastName";
+
+class App extends React.Component {
+  render() {
+    const firstNames = ["Greve", "Kim", "Solheim", "Sohn"];
+    return;
+  }
+}
+
+export default App;
+
+// ## Exercise 2
+
+// Create a parent class component inside which you define two arrays of equal length, the first will have 5 firstnames and the second 5 lastnames.
+
+// Create 2 children class components – one to display the firstnames and one to display the lastnames so that they are rendered side by side with matching order.
