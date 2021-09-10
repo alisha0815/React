99593f43206534794adf8b81c9d209ea0b9c3ec3
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-09-10 10:27:22 +0200

    how many times clicked button added using callback function

--- block03/bl03_playground/src/App.js
+++ block03/bl03_playground/src/App.js
@@ -8,13 +8,24 @@ class App extends React.Component {
   state = {
     name: "Alisha",
     city: "Oslo",
+    counter: 0,
   };
   year = "3000";
+
   buttonClicked = () => {
     console.log("Button clicked! 😎");
     this.setState({ city: this.state.city });
     this.year = 2020;
     console.log(this.year);
+  };
+
+  count = () => {
+    let { counter } = this.state;
+    counter += 1;
+    this.setState({ counter: counter }, () => {
+      console.log("updated counter in state==>", this.state.counter);
+    });
+    // console.log("updated counter in state==>", this.state.counter);
   };
 
   render() {
@@ -23,6 +34,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
+        <h1 onClick={this.count}>You clicked me {this.state.counter} times</h1>
         <h1>
           {this.state.name} lives in {this.state.city}
         </h1>
