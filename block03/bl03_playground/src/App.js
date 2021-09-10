3cc45ef23a4ae0a2ea5ae2fed8ed13cc4a726035
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-09-10 10:40:22 +0200

    getting the data before submitting by using onChange function

--- block03/bl03_playground/src/App.js
+++ block03/bl03_playground/src/App.js
@@ -9,6 +9,7 @@ class App extends React.Component {
     name: "Alisha",
     city: "Oslo",
     counter: 0,
+    userInput: "",
   };
   year = "3000";
 
@@ -28,12 +29,24 @@ class App extends React.Component {
     // console.log("updated counter in state==>", this.state.counter);
   };
 
+  handleChange = (event) => {
+    let data = event.target.value;
+    data.length > 6 ? console.log("good enough!") : console.log("keep typing");
+    this.setState({ userInput: data });
+    // console.log("from the input ==>", data.length);
+    // console.log("form the input==>", data);
+    // console.log("form the input==>", event);
+    // console.log("form the input==>", event.target);
+  };
+
   render() {
     let year = 2021;
 
     return (
       <div className="App">
         <Header />
+        <input onChange={this.handleChange} />
+        <p>you typed : {this.state.userInput}</p>
         <h1 onClick={this.count}>You clicked me {this.state.counter} times</h1>
         <h1>
           {this.state.name} lives in {this.state.city}
