de9f8bf219edacd5fdadc566239bac884b4ecc19
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-09-12 10:30:03 +0200

    userstate added

--- block04/bl04_1_playground/src/App.js
+++ block04/bl04_1_playground/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  // initial value 0
+  const [username, setUsername] = useState("");
+
+  let clicked = () => {
+    setCounter(counter + 1);
+  };
+
+  let handleInput = (event) => {
+    setUsername(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>You clicked the button {counter} times</h1>
+      <button onClick={clicked}>Click me!</button>
+      <input onChange={handleInput} />
+      <h1>{username}</h1>
     </div>
   );
 }
