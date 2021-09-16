18676349537bba093a2b2f5fdd05d98868388548
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-09-16 09:03:32 +0200

    events function added

--- block03/tweet_app/src/App.js
+++ block03/tweet_app/src/App.js
@@ -6,9 +6,20 @@ const App = () => {
   const name = "Alisha";
   const message = "Jeg heter Alisha";
 
+  const sayHelloHandler = (user) => {
+    alert(`Hello there ${user}`);
+    console.log(user);
+  };
+
+  const inputHandler = (e) => {
+    console.log(e);
+  };
+
   return (
     <div>
       <h1>Hello React!</h1>
+      <button onClick={() => sayHelloHandler("Mike")}>Click!!</button>
+      <button onClick={inputHandler}>Try Me</button>
       <CreateTweet />
       <TweetList name={name} message={message} />
     </div>
