d1188313e3fec8c2cacc7d8598838635943f864f
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-09-16 16:51:18 +0200

    search for movies axios using js function

--- block04/bl04_axios_playground/src/App.js
+++ block04/bl04_axios_playground/src/App.js
@@ -1,55 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-class App extends React.Component {
-  state = {
-    title: "",
-    year: "",
-    error: "",
-  };
+const App = () => {
+  // state = {
+  //   title: "",
+  //   year: "",
+  //   error: "",
+  // };
 
-  componentDidMount() {
-    this.findMovie("It");
-  }
+  // const [title, setTitle] = useState("");
+  // const [year, setYear] = useState("");
+  const [movie, setMovie] = useState();
+  // movie will be the object
+  const [error, setError] = useState("");
+  const [inputMovie, setInputMovie] = useState("");
+  // const [plot, setPlot] = useState("");
 
-  handleChange = (e) => {
+  // componentDidMount() {
+  //   this.findMovie("It");
+  // }
+
+  useEffect(() => {
+    findMovie("It");
+  }, []);
+
+  let handleChange = (e) => {
     let movie = e.target.value;
-    this.setState({ movieToSearch: movie });
+    // this.setState({ movieToSearch: movie });
+    // setMovie({ title: movie });
+    setInputMovie(movie);
   };
 
-  searchMovie = (e) => {
+  let searchMovie = (e) => {
     e.preventDefault();
-    this.findMovie(this.state.movieToSearch);
+    // this.findMovie(this.state.movieToSearch);
+    findMovie(inputMovie);
   };
 
-  findMovie = (movie) => {
+  let findMovie = (movie) => {
     console.log("from findMovie");
     let url = `http://www.omdbapi.com/?t=${movie}&apikey=5647a44b`;
     axios
       .get(url)
       .then((res) => {
         console.log(res.data);
-        // debugger;
-        this.setState({ title: res.data.Title, year: res.data.Year });
+        let { Title, Year, Plot } = res.data;
+        // this.setState({ title: res.data.Title, year: res.data.Year });
+        setMovie({ title: Title, year: Year, plot: Plot });
       })
       .catch((e) => {
         debugger;
       });
   };
-  render() {
-    return (
-      <div>
-        <h1>Search for</h1>
-        <form onSubmit={this.searchMovie}>
-          <input onChange={this.handleChange} />
-          <button>Search Movies</button>
-        </form>
-        <h1>
-          Title: {this.state.title} released year: {this.state.year}
-        </h1>
-      </div>
-    );
-  }
-}
+  // render() {
+  return (
+    <div>
+      <h1>Search for</h1>
+      <form onSubmit={searchMovie}>
+        <input onChange={handleChange} />
+        <button>Search Movies</button>
+      </form>
+      {movie ? (
+        <>
+          <h1>
+            Title: {movie.title}, release year: {movie.year}
+          </h1>
+          <p>{movie.plot}</p>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+// }
 
 export default App;
