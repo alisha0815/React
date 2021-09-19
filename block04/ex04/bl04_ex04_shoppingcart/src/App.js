538fa54a73996298ef9d422430fddbf91d83f528
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-09-19 15:07:53 +0200

    shopping cart basic structure setup

---
+++ block04/ex04/bl04_ex04_shoppingcart/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Basket from "./components/Basket";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className="row">
+        <Main />
+        <Basket />
+      </div>
+    </div>
+  );
+}
+
+export default App;
